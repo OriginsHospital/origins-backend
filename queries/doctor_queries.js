7808c8003092b9f59aab06d982f010f4c4d8751e
NIKHILSUVVA77 <nikhilsuvva77@gmail.com>
2026-09-04 00:11:40 +0530

    app

--- queries/doctor_queries.js
+++ queries/doctor_queries.js
@@ -139,7 +139,6 @@ WITH appointments as (
         caa.consultationDoctorId = :doctorId
         AND caa.appointmentDate = :date
         AND caa.noShow = 0
-        AND pva.isActive = 1
     UNION ALL
     SELECT
         taa.id as appointmentId,
@@ -242,7 +241,6 @@ WITH appointments as (
         taa.consultationDoctorId = :doctorId
         AND taa.appointmentDate = :date
         AND taa.noShow = 0
-        AND pva.isActive = 1
     )
     select * from appointments where stage IN ('Doctor', 'Seen', 'Done') order by isCompleted ASC ,timeStart ASC ;
 `;
